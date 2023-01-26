0a3f208dc381aec7a95eab3bc457f4e047395d7d
MamyGhost <80816758+MamyGhost@users.noreply.github.com>
2023-01-26 17:51:19 +0300

    update design + fonction Finance

--- Backend/app/controllers/finance.controller.js
+++ Backend/app/controllers/finance.controller.js
@@ -16,18 +16,20 @@ exports.getStat = async (req, res) => {
   let statchi = await service.getStatchiffreA();
   let statdep = await service.getStatdepense();
   let avgReparation = await service.getAvgReparation();
+  let benefice = null;
   if (statchi.length > 0 && statdep[0].length > 0) {
     delete statchi[0]._id;
     delete statdep[0]._id;
+      //iterate over map2 entries with acc set to map1 at start
+   benefice = Object.entries(statdep[0]).reduce((acc, [key, value]) =>
+  // if key is already in map1, add the values, otherwise, create new pair
+  ({ ...acc, [key]: (acc[key] || 0) - value })
+  , { ...statchi[0] });
   }
 
 
 
-  //iterate over map2 entries with acc set to map1 at start
-  const benefice = Object.entries(statdep[0]).reduce((acc, [key, value]) =>
-    // if key is already in map1, add the values, otherwise, create new pair
-    ({ ...acc, [key]: (acc[key] || 0) - value })
-    , { ...statchi[0] });
+
 
   const initial = { janvier: 0, fevrier: 0, mars: 0, avril: 0, mai: 0, juin: 0, juillet: 0, aout: 0, septembre: 0, octobre: 0, novembre: 0, decembre: 0 };
 
@@ -38,7 +40,7 @@ exports.getStat = async (req, res) => {
     depenseAn: depan[0]?.totalDepensesAn ?? 0,
     statchi: statchi[0] ?? initial,
     statbenefice: benefice ?? initial,
-    avgReparation: avgReparation[0].averageTime ?? 0
+    avgReparation: avgReparation[0]?.averageTime ?? 0
 
   };
   res.send(stat);
