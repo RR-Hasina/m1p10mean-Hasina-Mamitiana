2b71bf584dc1db7d1f534e476e31d6923b9e87d3
Orion <nyhasinaramamonjy@gmail.com>
2023-01-27 12:57:22 +0300

    Front: update topbar

--- Backend/app/services/email.service.js
+++ Backend/app/services/email.service.js
@@ -12,59 +12,58 @@ const frontURL = "http://localhost:4200";
 
 // Create and initialize the email transport object
 const transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "c363cd7b0f371c",
-      pass: "4a97548484ee2b"
-    }
-  });
-
-  exports.isEmailValid = async(email) =>{
-    return emailValidator.validate(email)
+  service:"Gmail",
+  auth: {
+    user: "etechorion@gmail.com",
+    pass: "fmloznfqzwjbqtqf"
   }
+});
 
-exports.sendEmailReparation = async (req,res) => {
-    try {
-          //Load the template file
-          const templateFile = fs.readFileSync((path.join(__dirname, '..')+"\\templates\\reparationMail\\reparationMail.html"));
-          //Load and inline the style
-          const templateStyled = await inlineCss(templateFile.toString(), {url:  "file://"+(path.join(__dirname, '..')+"\\templates\\reparationMail\\")});
-          //Inject the data in the template and compile the html
-          const templateCompiled = handlebars.compile(templateStyled);
-          const templateRendered = templateCompiled({user :req.body.user,marque : req.body.marque,imm:req.params.imm});
-  
-          const emailData = { 
-              to: "receiver@example.com", 
-              from: '"Example" sender@example.com', 
-              subject: "Réparation de la voiture "+req.body.marque+", imm: "+req.params.imm, 
-              html: templateRendered 
-          };
+exports.isEmailValid = async (email) => {
+  return emailValidator.validate(email)
+}
 
-        //Send the email
-        transporter.sendMail(emailData, function(error, info){
-            if (error) {
-              res.status(500).send(error);
-            } else {
-              res.status(200).send({message:"Composant mis à jour et Email envoyer"});
-            }
-          });
+exports.sendEmailReparation = async (req, res) => {
+  try {
+    //Load the template file
+    const templateFile = fs.readFileSync((path.join(__dirname, '..') + "\\templates\\reparationMail\\reparationMail.html"));
+    //Load and inline the style
+    const templateStyled = await inlineCss(templateFile.toString(), { url: "file://" + (path.join(__dirname, '..') + "\\templates\\reparationMail\\") });
+    //Inject the data in the template and compile the html
+    const templateCompiled = handlebars.compile(templateStyled);
+    const templateRendered = templateCompiled({ user: req.body.user, marque: req.body.marque, imm: req.params.imm });
 
-        } catch(e){
-            console.error(e);
-        }      
- }
+    const emailData = {
+      to: "receiver@example.com",
+      from: '"Example" sender@example.com',
+      subject: "Réparation de la voiture " + req.body.marque + ", imm: " + req.params.imm,
+      html: templateRendered
+    };
 
- exports.sendConfirmationEmail = (name, email, confirmationCode) => {
-    console.log("Check");
-    transporter.sendMail({
-      from: 'c363cd7b0f371c',
-      to: email,
-      subject: "Please confirm your account",
-      html: `<h1>Email Confirmation</h1>
+    //Send the email
+    transporter.sendMail(emailData, function (error, info) {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send({ message: "Composant mis à jour et Email envoyer" });
+      }
+    });
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+  console.log("Check");
+  transporter.sendMail({
+    from: 'c363cd7b0f371c',
+    to: email,
+    subject: "Please confirm your account",
+    html: `<h1>Email Confirmation</h1>
           <h2>Hello ${name}</h2>
           <p>Merci de vous être abonné. Veuillez confirmer votre email en cliquant sur le lien suivant</p>
           <a href=${frontURL}/confirm/${confirmationCode}> Cliquez ici</a>
           </div>`,
-    }).catch(err => console.log(err));
-  };
+  }).catch(err => console.log(err));
+};
--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ require("dotenv/config");
 const app = express();
 
 app.use(cors({
-  origin: "https://reparation-auto-back.onrender.com",
+  origin: "http://localhost:4200",
   credentials: true
 }));
 app.use(express.json());
