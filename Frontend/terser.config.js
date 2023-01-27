43926493d0f6ffc180492ddab96401808ed724f2
Orion <nyhasinaramamonjy@gmail.com>
2023-01-27 21:32:37 +0300

    Front: update

---
+++ Frontend/terser.config.js
@@ -0,0 +1,16 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+};
