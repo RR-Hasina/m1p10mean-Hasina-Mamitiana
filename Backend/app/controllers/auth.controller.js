6aae425d3a3492bed4b7ac0998200de6c4a0f9d9
MamyGhost <80816758+MamyGhost@users.noreply.github.com>
2023-01-29 11:51:38 +0300

    Update auth.controller.js

--- Backend/app/controllers/auth.controller.js
+++ Backend/app/controllers/auth.controller.js
@@ -9,7 +9,7 @@ var CryptoJS = require("crypto-js");
 
 const serviceMail = require("../services/email.service");
 
-exports.signup = async(req, res) => {
+exports.signup = async (req, res) => {
 
   const token = jwt.sign({email: req.body.email}, process.env.JWT_SECRET);
   const user = new User({
@@ -21,17 +21,16 @@ exports.signup = async(req, res) => {
     confirmationCode: token
   });
 
-  user.save((err, user) => {
+    user.save( async (err, user) => {
     if (err) {
       res.status(500).send({ message: err });
       return;
     }
     res.send({ message: "l\'utilisateur a été enregistré!" });
-
-      serviceMail.sendConfirmationEmail(
-      user.prenom,
-      user.email,
-      user.confirmationCode
+    await serviceMail.sendConfirmationEmail(
+      req.body.prenom,
+      req.body.email,
+      token
     )
   });
 };
